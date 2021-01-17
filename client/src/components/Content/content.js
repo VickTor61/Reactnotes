c81617f790ef95d9c6063a21c22cb54e9d442f37
Deborah Enoma <enomaomorogieva@gmail.com>
2021-01-16 21:47:44 -0500

    Update routes

--- client/src/components/Content/content.js
+++ client/src/components/Content/content.js
@@ -4,26 +4,25 @@ import axios from "axios";
 import { useHistory } from "react-router";
 
 function Content() {
-
-  const History = useHistory()
+  const History = useHistory();
 
   const [postData, setpostData] = useState({
     firstName: "",
     lastName: "",
-    email: "",
+    email: ""
   });
 
-  const updateName = (e) => {
+  const updateName = e => {
     const { value, name } = e.target;
-    setpostData((prev) => {
+    setpostData(prev => {
       return {
         ...prev,
-        [name]: value,
+        [name]: value
       };
     });
   };
 
-  const postDataHandler = (e) => {
+  const postDataHandler = e => {
     e.preventDefault();
     console.log(postData);
     if (
@@ -31,46 +30,45 @@ function Content() {
       postData.lastName.length > 0 &&
       postData.email.length > 0
     ) {
-    
-      axios.post("/users", postData);
+      axios.post("/api/users", postData);
     }
     History.push("/");
   };
 
   return (
-    <div className='new__user'>
-      <h1 className='new__user-heading'>Add new user</h1>
+    <div className="new__user">
+      <h1 className="new__user-heading">Add new user</h1>
       <form>
         <input
-          className='new__user-input'
+          className="new__user-input"
           onChange={updateName}
           value={postData.firstName}
-          name='firstName'
-          placeholder='Firstname'
+          name="firstName"
+          placeholder="Firstname"
           required
         />
 
         <input
-          className='new__user-input'
+          className="new__user-input"
           onChange={updateName}
           value={postData.lastName}
-          name='lastName'
-          placeholder='Lastname'
+          name="lastName"
+          placeholder="Lastname"
           required
         />
 
         <input
-          className='new__user-input'
+          className="new__user-input"
           value={postData.email}
           onChange={updateName}
-          name='email'
-          placeholder='Email'
+          name="email"
+          placeholder="Email"
           required
         />
 
         <button
-          type='submit'
-          className='new__user-btn'
+          type="submit"
+          className="new__user-btn"
           onClick={postDataHandler}
         >
           create User
